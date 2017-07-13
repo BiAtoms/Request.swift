77244087c632a2088f77453a80142187120aeec7
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2017-07-14 02:32:17 +0400

    Added request errors.

---
+++ Sources/RequestError.swift
@@ -0,0 +1,18 @@
+//
+//  RequestError.swift
+//  RequestSwift
+//
+//  Created by Orkhan Alikhanov on 7/14/17.
+//  Copyright © 2017 BiAtoms. All rights reserved.
+//
+
+import Foundation
+
+extension Request {
+    
+    public enum Error: Swift.Error {
+        case timeout
+        case couldntResolveHost
+    }
+    
+}
