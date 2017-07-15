0fa3563f6f92608300be1d2873ef7085163a7c78
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2017-07-15 13:53:32 +0400

    Added using system proxy. (Except for linux)

--- Sources/Client.swift
+++ Sources/Client.swift
@@ -18,10 +18,38 @@ open class Client {
     let queue = DispatchQueue(label: "com.biatoms.request-swift." + UUID().uuidString)
     var firesImmediately: Bool = true
     var timeout: Int = 5000 // in ms
+    
+    var usesSystemProxy: Bool = true
     var proxy: Proxy? = nil
     
-    public init(baseUrl: String? = nil) {
+    public init(baseUrl: String? = nil, usesSystemProxy: Bool = true) {
         self.baseUrl = baseUrl
+    }
+    
+    //https://forums.developer.apple.com/thread/65416
+    open static func getSystemProxy(for host: String) -> Proxy? {
+        #if os(Linux) //CFNetworkCopySystemProxySettings hasn't been implemented. (see https://github.com/apple/swift-corelibs-foundation)
+        #else
+        if let url = URL(string: host) {
+            if let proxySettingsUnmanaged = CFNetworkCopySystemProxySettings() {
+                let proxySettings = proxySettingsUnmanaged.takeRetainedValue()
+                let proxiesUnmanaged = CFNetworkCopyProxiesForURL(url as CFURL, proxySettings)
+                let proxies = proxiesUnmanaged.takeRetainedValue() as! [[String:AnyObject]]
+                
+                for dict in proxies {
+                    if let type = dict[kCFProxyTypeKey as String] {
+                        if type as! CFString  == kCFProxyTypeHTTP { //only http for now
+                            let host = dict[kCFProxyHostNameKey as String] as! String
+                            let port = (dict[kCFProxyPortNumberKey as String] as! NSNumber).intValue
+                            return (host, Port(port))
+                        }
+                    }
+                }
+                
+            }   
+        }
+        #endif
+        return nil
     }
     
     
@@ -42,6 +70,11 @@ open class Client {
         
         let request = Request(method: method, url: url, headers: headers ?? [:], body: [])
         encoding.encode(request, with: parameters)
+        var proxy = self.proxy
+        if usesSystemProxy {
+            proxy = Client.getSystemProxy(for: url)
+        }
+        
         let requester = Requester(request: request, queue: queue, timeout: timeout, proxy: proxy)
         
         if firesImmediately {
